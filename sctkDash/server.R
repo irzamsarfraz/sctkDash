0cdb51786ef4aeaeb98e813e3275c0cb46b62101
Irzam Sarfraz <irzam9095@gmail.com>
2021-05-23 15:23:22 +0500

    demo

--- sctkDash/server.R
+++ sctkDash/server.R
@@ -8,19 +8,52 @@
 #
 
 library(shiny)
-
+library(shinydashboard)
+library(shinydashboardPlus)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+  output$plot <- renderPlot({
+    req(!input$mybox$collapsed)
+    plot(rnorm(200))
+  })
+  
+  output$box_state <- renderText({
+    state <- if (input$mybox$collapsed) "collapsed" else "uncollapsed"
+    paste("My box is", state)
+  })
+  
+  observeEvent(input$toggle_box, {
+    updateBox("mybox", action = "toggle")
+  })
+  
+  observeEvent(input$remove_box, {
+    updateBox("mybox", action = "remove")
+  })
+  
+  observeEvent(input$restore_box, {
+    updateBox("mybox", action = "restore")
+  })
+  
+  observeEvent(input$update_box, {
+    updateBox(
+      "mybox", 
+      action = "update", 
+      options = list(
+        title = h2("New title", dashboardLabel(1, status = "primary")),
+        status = "danger", 
+        solidHeader = TRUE,
+        width = 4
+      )
+    )
+  })
+  
+  observeEvent(input$mybox$visible, {
+    collapsed <- if (input$mybox$collapsed) "collapsed" else "uncollapsed"
+    visible <- if (input$mybox$visible) "visible" else "hidden"
+    message <- paste("My box is", collapsed, "and", visible)
+    showNotification(message, type = "warning", duration = 1)
+  })
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
 
 })
--- sctkDash/ui.R
+++ sctkDash/ui.R
@@ -8,26 +8,184 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinyBS)
+library(shinyWidgets)
+library(shiny)
+library(shinyjs)
+library(shinyFiles)
+library(ComplexHeatmap)
+library(limma)
+library(ggplot2)
+library(plotly)
+library(data.table)
+library(colourpicker)
+library(gridExtra)
+library(cluster)
+library(ggtree)
+library(ape)
+library(GSVA)
+library(GSVAdata)
+library(shinyalert)
+library(enrichR)
+library(matrixStats)
+library(Biobase)
+library(base)
+library(SingleCellExperiment)
+library(singleCellTK)
+library(celda)
+library(shinycssloaders)
+library(shinythemes)
+library(shinyBS);
+library(shinyjqui);
+library(Seurat);
+library(ggplotify);
+library(ggplot2);
+library(cowplot);
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(broom)
+library(RColorBrewer)
+library(grDevices)
+library(shinyWidgets)
+library(stringr)
+library(Hmisc)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+shinyUI(
+    dashboardPage(
+        title = "Box API",
+        dashboardHeader(),
+        sidebar = dashboardSidebar(
+            sidebarMenu(
+                menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+                menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+                         badgeLabel = "new", badgeColor = "green")
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        body = dashboardBody(
+            tabItems(
+                tabItem(tabName = "dashboard",
+                        h2("Dashboard tab content"),
+                        fluidRow(
+                            tabBox(
+                                title = "",
+                                # The id lets us use input$tabset1 on the server to find the current tab
+                                id = "tabset1", height = "250px", width = NULL,
+                                tabPanel("Feature Selection", "First tab content",
+                                         fluidRow(
+                                             box(title = "Plot", plotOutput("abc"), background = "gray", sidebar = boxSidebar(
+                                                 id = "mycardsidebar",
+                                                 width = 25,
+                                                 sliderInput(
+                                                     "obs", 
+                                                     "Number of observations:",
+                                                     min = 0, 
+                                                     max = 1000, 
+                                                     value = 500
+                                                 )
+                                             )),
+                                             box(title = "Options", textInput("asd", "Label:"), actionButton("abcs", "Run"), background = "gray", collapsible = TRUE)
+                                         )),
+                                tabPanel("Dimensionality Reduction", "Tab content 2")
+                            )
+                        )
+                ),
+                
+                tabItem(tabName = "widgets",
+                        h2("Widgets tab content"),
+                        bsCollapse(id = "SeuratUI", open = "Data Input",
+                                   bsCollapsePanel("Normalize Data"),
+                                   bsCollapsePanel("Scale Data"),
+                                   bsCollapsePanel("Highly Variable Genes"),
+                                   bsCollapsePanel("Dimensionality Reduction",
+                                                   tabsetPanel(type = "tabs",
+                                                               tabPanel("PCA",
+                                                                        br(),
+                                                                        fluidRow(
+                                                                            column(4,
+                                                                                   fluidRow(
+                                                                                       column(12,
+                                                                                              panel(heading = "PCA",
+                                                                                                    textInput(inputId = "pca_no_components", label = "Select number of components to compute: ", value = "50"),
+                                                                                                    materialSwitch(inputId = "pca_compute_elbow", label = "Compute ElbowPlot?", value = TRUE),
+                                                                                                    materialSwitch(inputId = "pca_compute_jackstraw", label = "Compute JackStrawPlot?", value = FALSE),
+                                                                                                    materialSwitch(inputId = "pca_compute_heatmap", label = "Compute Heatmap?", value = TRUE),
+                                                                                                    conditionalPanel(
+                                                                                                        condition = 'input.pca_compute_heatmap == true',
+                                                                                                        numericInput(inputId = "pca_compute_heatmap_nfeatures",
+                                                                                                                     label = "Set number of features for heatmap:", value = 30, step = 1),
+                                                                                                    ),
+                                                                                                    actionButton(inputId = "run_pca_button", "Run PCA")
+                                                                                              ),
+                                                                                              panel(heading = "Select No. of Components",
+                                                                                                    htmlOutput(outputId = "pca_significant_pc_output", inline = FALSE),
+                                                                                                    numericInput(inputId = "pca_significant_pc_counter", label = "Select number of components for downstream analysis: ", min = 1, max = 20, value = 10)
+                                                                                              )
+                                                                                       )
+                                                                                   )
+                                                                            ),
+                                                                            column(8,
+                                                                                   fluidRow(
+                                                                                       column(12,
+                                                                                              hidden(
+                                                                                                  tags$div(class = "seurat_pca_plots", tabsetPanel(id = "seuratPCAPlotTabset", type = "tabs"
+                                                                                                  )
+                                                                                                  ))
+                                                                                       )
+                                                                                       
+                                                                                   )
+                                                                            )
+                                                                        )
+                                                                        
+                                                               ),
+                                                               tabPanel("ICA",
+                                                                        br(),
+                                                                        fluidRow(
+                                                                            column(4,
+                                                                                   fluidRow(
+                                                                                       column(12,
+                                                                                              panel(heading = "ICA",
+                                                                                                    textInput(inputId = "ica_no_components", label = "Select number of components to compute: ", value = "20"),
+                                                                                                    materialSwitch(inputId = "ica_compute_heatmap", label = "Compute Heatmap?", value = TRUE),
+                                                                                                    conditionalPanel(
+                                                                                                        condition = 'input.ica_compute_heatmap == true',
+                                                                                                        numericInput(inputId = "ica_compute_heatmap_nfeatures",
+                                                                                                                     label = "Set number of features for heatmap:", value = 30, step = 1),
+                                                                                                    ),
+                                                                                                    actionButton(inputId = "run_ica_button", "Run ICA")
+                                                                                              ),
+                                                                                              panel(heading = "Select No. of Components",
+                                                                                                    #h5("Number of components suggested by ElbowPlot: "),
+                                                                                                    #verbatimTextOutput(outputId = "ica_significant_pc_output", placeholder = TRUE),
+                                                                                                    numericInput(inputId = "ica_significant_ic_counter", label = "Select number of components for downstream analysis: ", min = 1, max = 20, value = 10)
+                                                                                              )
+                                                                                       )
+                                                                                   )
+                                                                            ),
+                                                                            column(8,
+                                                                                   fluidRow(
+                                                                                       column(12,
+                                                                                              hidden(
+                                                                                                  tags$div(class = "seurat_ica_plots", tabsetPanel(id="seuratICAPlotTabset", type = "tabs"
+                                                                                                  ))
+                                                                                              )
+                                                                                       )
+                                                                                   )
+                                                                            )
+                                                                        )
+                                                                        
+                                                               )
+                                                   ),
+                                                   style = "primary")
+                        )
+                )
+            )
         )
+        
     )
-))
+)
+
+
